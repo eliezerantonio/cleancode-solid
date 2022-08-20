dfcbb12cca70a7faa9e671b5e4fc188363e1c232
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-08-20 22:41:39 +0100

    functions

--- src/clean-code/03-funciones.ts
+++ src/clean-code/03-funciones.ts
@@ -1,33 +1,40 @@
 (() => {
 
-    // función para obtener información de una película por Id
-    function getMovieById( movieId: string ) {
-        console.log({ movieId });
-    }
-
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
+    function getMovieById( id: string ) {
         console.log({ id });
     }
 
-    // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
-        console.log({ ActorId });
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    function getActorBioById( id: string ) {
+        console.log({ id });
     }
     
-    // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+
+    interface Movie{
+        cast: string[];
+        description: string;
+        rating: number;   
+        title: string;
+
+    }
+    function createMovie({title,  description, rating ,cast }:Movie) {
         console.log({ title, description, rating, cast });
     }
 
-    // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
-        
-        // tarea asincrona para verificar nombre
-        // ..
-        // ..
-        if ( fullName === 'fernando' ) return false;
 
+    function checkFullName(fullName: string) {
+        console.log({ fullName });
+        
+        return true;  
+
+    }
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        
+        if (checkFullName(fullName)) return false;
         console.log('Crear actor');
         return true;        
 
